243c9ff23e3b1d9c7e0bdc6237ab55239b29f2a1
TOprisan <tzeusd@gmail.com>
2015-02-19 22:42:49 +0200

    InitialCommit

---
+++ lab1/src/MyRekt.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Doru on 2/19/2015.
+ */
+public class MyRekt extends Rectangle implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public MyRekt(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+}
